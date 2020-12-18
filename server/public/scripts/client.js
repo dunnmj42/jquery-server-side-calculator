2bf9c738b0ecd20a466011e748b1c950acda984e
Michael Dunn <dunnmj42@gmail.com>
2020-12-18 16:50:36 -0600

    object construction in client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,37 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log("jquery is loaded!");
-  
-  }
-  
+    
+    $('#plusBtn').on('click', plusBtn);
+    $('#subBtn').on('click', subBtn);
+    $('#multBtn').on('click', multBtn);
+    $('#diviBtn').on('click', diviBtn);
+    $('#eqBtn').on('click', eqBtn);
+    $('#clrBtn').on('click', clrBtn);
+}
+
+let equation = {};
+
+function plusBtn() {
+    equation.opperator = '+'
+}
+
+function subBtn() {
+    equation.opperator = '-'
+}
+
+function multBtn() {
+    equation.opperator = '*'
+}
+
+function diviBtn() {
+    equation.opperator = '/'
+}
+
+function eqBtn() {
+    
+    equation.firstNum = $('#firstNum').val(),
+    equation.secondNum = $('#secondNum').val(),
+    
+    console.log(equation);
+}
