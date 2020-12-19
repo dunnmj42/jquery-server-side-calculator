39d4271b830e9ca974edd407c8a3d60c3f961000
Michael Dunn <dunnmj42@gmail.com>
2020-12-18 19:05:02 -0600

    prettier code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,81 +1,77 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-    console.log("jquery is loaded!");
-    
-    $('#plusBtn').on('click', plusBtn);
-    $('#subBtn').on('click', subBtn);
-    $('#multBtn').on('click', multBtn);
-    $('#diviBtn').on('click', diviBtn);
-    $('#eqBtn').on('click', eqBtn);
-    $('#clrBtn').on('click', clrBtn);
+  console.log("jquery is loaded!");
+
+  $("#plusBtn").on("click", plusBtn);
+  $("#subBtn").on("click", subBtn);
+  $("#multBtn").on("click", multBtn);
+  $("#diviBtn").on("click", diviBtn);
+  $("#eqBtn").on("click", eqBtn);
+  $("#clrBtn").on("click", clrBtn);
 }
 
 function renderToDom() {
-    $.ajax({
-        url : '/calculate',
-        type : 'GET'
-    }).then(function(response){
-        
-        let history = response;
-        let lastAnswer = history[history.length - 1].result;
+  $.ajax({
+    url: "/calculate",
+    type: "GET",
+  }).then(function (response) {
+    let history = response;
+    let lastAnswer = history[history.length - 1].result;
 
-        console.log(history);
-        
-        $('#calcAnswer').text(`${lastAnswer}`)
-        $('#historyList').empty();
+    console.log(history);
 
-        for (let i = 0; i < history.length; i++) {
-            
-            $('#historyList').append(`<li>
+    $("#calcAnswer").text(`${lastAnswer}`);
+    $("#historyList").empty();
+
+    for (let i = 0; i < history.length; i++) {
+      $("#historyList").append(`<li>
             ${history[i].firstNum}
             ${history[i].opperator}
             ${history[i].secondNum} = 
             ${history[i].result}</li>
             `);
-        }
-    });
+    }
+  });
 }
 
 let equation = {};
 
 function plusBtn() {
-    equation.opperator = '+'
+  equation.opperator = "+";
 }
 
 function subBtn() {
-    equation.opperator = '-'
+  equation.opperator = "-";
 }
 
 function multBtn() {
-    equation.opperator = '*'
+  equation.opperator = "*";
 }
 
 function diviBtn() {
-    equation.opperator = '/'
+  equation.opperator = "/";
 }
 
 function eqBtn() {
-    
-    equation.firstNum = $('#firstNum').val(),
-    equation.secondNum = $('#secondNum').val(),
-    
+  (equation.firstNum = $("#firstNum").val()),
+    (equation.secondNum = $("#secondNum").val()),
     console.log(equation);
 
-    $.ajax({
-        url: '/calculate',
-        type: 'POST',
-        data: equation
-    }).then(function(response) {     
-        console.log(response);
-        renderToDom();
-    });
+  $.ajax({
+    url: "/calculate",
+    type: "POST",
+    data: equation,
+  }).then(function (response) {
+    console.log(response);
+    renderToDom();
+  });
 }
 
 function clrBtn() {
-    $('#firstNum').val('');
-    $('#secondNum').val('');
-    $('#calcAnswer').text('');
-    equation = {};
-    console.log(equation);
+  $("#firstNum").val("");
+  $("#secondNum").val("");
+  $("#calcAnswer").text("");
+  equation = {};
+  console.log(equation);
 }
--- server/server.js
+++ server/server.js
@@ -1,50 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
 
 let history = [];
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
-app.post('/calculate', (req, res) => {
+app.post("/calculate", (req, res) => {
+  let equation = req.body;
 
-    let equation = req.body;
-  
-    console.log(equation);
-    calculate(equation);
-    console.log(history);
-    res.sendStatus(201);
-  
+  console.log(equation);
+  calculate(equation);
+  console.log(history);
+  res.sendStatus(201);
 });
 
-app.get('/calculate', (req, res) => {
-    res.send(history);
+app.get("/calculate", (req, res) => {
+  res.send(history);
 });
 
 function calculate(equation) {
-    let opperator = equation.opperator
-    let firstNum = Number(equation.firstNum);
-    let secondNum = Number(equation.secondNum);
-    let result;
+  let opperator = equation.opperator;
+  let firstNum = Number(equation.firstNum);
+  let secondNum = Number(equation.secondNum);
+  let result;
 
-    if (opperator === '+') {
-        result = firstNum + secondNum
-    } else if (opperator === '-'){
-        result = firstNum - secondNum
-    } else if (opperator === '*'){
-        result = firstNum * secondNum
-    } else {
-        result = firstNum / secondNum
-    }
+  if (opperator === "+") {
+    result = firstNum + secondNum;
+  } else if (opperator === "-") {
+    result = firstNum - secondNum;
+  } else if (opperator === "*") {
+    result = firstNum * secondNum;
+  } else {
+    result = firstNum / secondNum;
+  }
 
-    equation.result = result;
-    history.push(equation)
-    
+  equation.result = result;
+  history.push(equation);
 }
 
 app.listen(PORT, () => {
-    console.log ('Server is running on port', PORT)
+  console.log("Server is running on port", PORT);
 });
